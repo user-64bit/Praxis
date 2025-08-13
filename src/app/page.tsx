1abeaff643dd93862e41bd9c6834e57e55fa6031
User-64bit <arth.prajapati@outlook.com>
2025-08-13 20:30:00 +0000

    new start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,8 @@
-"use client";
-
-import { ChatBox } from "@/components/chat-box";
-import { useState } from "react";
 
 export default function Home() {
-  // Todo: message will be fetched from the server if there are any
-  const [messages, setMessages] = useState<string[]>([]);
-  const handleSendMessage = (message: string) => {
-    setMessages(prev => [...prev, message]);
-    console.log('Message sent:', message);
-  };
-
   return (
-    <div className="min-h-screen bg-[#212020]">
-      <div className="flex flex-col items-center justify-center h-screen">
-        <ChatBox onSendMessage={handleSendMessage} />
-      </div>
+    <div>
+      <h1>Better Wallet</h1>
     </div>
   );
 }
